aca67a68dbacf6c1b52add2564501c244e41443e
Arnaud Huck <arnaud.huck@dgbes.com>
2021-08-31 10:56:23 +0200

    Made it compatible with dgbpy of OpendTect-6.6.X

--- webinars/2021-04-22/Examples/2a-get_default_models.py
+++ webinars/2021-04-22/Examples/2a-get_default_models.py
@@ -3,32 +3,71 @@
 This is a test script file.
 """
 
+import os
+
 from dgbpy import dgbkeras
 from dgbpy import mlio as dgbmlio
+from dgbpy import keystr as dgbkeys
 
-pickssetexfilenm = 'P:\\Git\\python-dgb-ml\\exampledata\\malenov_input_8x8x16_subsample_dBG.h5'
+expath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data')
+
+def getExampleInfos( infodict ):
+    learntype = infodict[dgbkeys.learntypedictstr]
+    classification = infodict[dgbkeys.classdictstr]
+    inpshape = infodict[dgbkeys.inpshapedictstr]
+    if isinstance(inpshape,int):
+        nrdims = inpshape
+    else:
+        nrdims = len(inpshape)
+    return (learntype,classification,nrdims)
+
+pickssetexfilenm = os.path.join(expath,'malenov_input_8x8x16_subsample_dBG.h5')
 pickssetinfos = dgbmlio.getInfo(pickssetexfilenm, quick=True)
-pickssetmodel = dgbkeras.getDefaultModel(
-    pickssetinfos, type=dgbkeras.mltypes[dgbkeras.letnetidx][0])
+(learntype,classification,nrdims) = getExampleInfos( pickssetinfos )
+modeltype = dgbkeras.getModelsByType( learntype, classification, nrdims )
+pickssetmodel = dgbkeras.getDefaultModel( pickssetinfos, type=modeltype[0] )
 pickssetmodel.summary()
 
-
-wllexfilenm = 'P:\\Git\\python-dgb-ml\\exampledata\\Log_-_Lithology_supervised_prediction.h5'
+wllexfilenm = os.path.join(expath,'Log_-_Lithology_supervised_prediction.h5')
 wllinfos = dgbmlio.getInfo(wllexfilenm, quick=True)
-wllmodel = dgbkeras.getDefaultModel(
-    wllinfos, type=dgbkeras.mltypes[dgbkeras.letnetidx][0])
+(learntype,classification,nrdims) = getExampleInfos( wllinfos )
+modeltype = dgbkeras.getModelsByType( learntype, classification, nrdims )
+wllmodel = dgbkeras.getDefaultModel( wllinfos, type=modeltype[0] )
 wllmodel.summary()
 
-imgexfilenm = 'P:\\Git\\python-dgb-ml\\exampledata\\Layers_input_2D.h5'
+imgexfilenm = os.path.join(expath,'Layers_input_2D.h5')
 imginfos = dgbmlio.getInfo(imgexfilenm, quick=True)
-imgmodel = dgbkeras.getDefaultModel(
-    imginfos, type=dgbkeras.mltypes[dgbkeras.unetidx][0])
+(learntype,classification,nrdims) = getExampleInfos( imginfos )
+modeltype = dgbkeras.getModelsByType( learntype, classification, nrdims )
+imgmodel = dgbkeras.getDefaultModel( imginfos, type=modeltype[0] )
 imgmodel.summary()
 
+import dgbpy.keras_classes as kc
+if len(kc.UserModel.mlmodels) < 1:
+    kc.UserModel.mlmodels = kc.UserModel.findModels()
 
-lenetmodel = dgbkeras.getDefaultLeNet(False, (5, 9, 17, 3), 1)
+learntype = dgbkeys.seisclasstypestr
+nrattribs = 3
+inp_shape = (5, 9, 17)
+model_shape = (*inp_shape,nrattribs)
+classification = True
+nrout = 9 # Number of labels
+
+data_format = 'channels_last'
+modeltype = dgbkeras.getModelsByType( learntype, classification, len(inp_shape) )
+learnrate = 1e-5
+lenetmodel = kc.UserModel.findName(modeltype[0]).model( model_shape, nrout, \
+                                            learnrate, data_format=data_format ) 
 lenetmodel.summary()
 
+learntype = dgbkeys.seisimgtoimgtypestr
+nrattribs = 1
+inp_shape = (128, 128, 128)
+model_shape = (*inp_shape,nrattribs)
+classification = False
+nrout = 1
 
-unetmodel = dgbkeras.getDefaultUnet(False, (128, 128, 128, 1), 1)
+modeltype = dgbkeras.getModelsByType( learntype, classification, len(inp_shape) )
+unetmodel = kc.UserModel.findName(modeltype[0]).model( model_shape, nrout, \
+                                            learnrate, data_format=data_format ) 
 unetmodel.summary()
